7598fffffab26648b20586f529ac608e8f2a771b
Jelaleddin Sultanov <jelaleddinsultanov@yandex.ru>
2021-12-10 21:05:37 +0500

    DVC linked to Heroku

--- fastapi1.py
+++ fastapi1.py
@@ -25,6 +25,12 @@ class Census(BaseModel):
 
 app = FastAPI()
 
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 @app.get('/')
 async def get():
     return {'message': 'Hello'}
